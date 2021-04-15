df49253e561c36b4957f577fd86fa45e6ea487aa
MOMachine <marcelomaortiz@hotmail.com>
2021-04-15 18:17:30 -0300

    Exemplo basico de API REST com node finalizado

---
+++ todoListApi/server.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+require('dotenv').config();
+  
+// mongoose instance connection url connection
+const mongodb_username = process.env['MONGODB_USERNAME'];
+const mongodb_password = process.env['MONGODB_PASSWORD'];
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb', {
+  "auth": {
+    "authSource": "admin"
+  },
+  "user": mongodb_username,
+  "pass": mongodb_password
+}); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
